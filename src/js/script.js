d3b0c32732896eef38f5252f568cf87f1ae25a28
Alex <example@gmail.com>
2021-07-15 22:15:52 +0300

    fist commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// let answer = confirm("Леха это ты ?")
+// if (answer) {
+//     console.log('Леша зайка')
+// } else
+//     console.log('Ксюша зайка')
+
+for (let i = 5; i < 3300; i++) {
+    console.log(i)
+}
